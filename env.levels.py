384a3c1a7a6af9c390878cd5322b3726ab7adece
VeronicaPistolesi <v.pistolesi6@studenti.unipi.it>
2023-12-15 19:03:54 +0100

    level1 static

---
+++ env.levels.py
@@ -0,0 +1,28 @@
+# CUSTOM FULLY OBSERVABLE ENVIRONMENTS CREATED WITH LEVEL EDITOR
+
+#LEVEL 1
+des_file_static_lv1 = """
+MAZE: "mylevel", ' '
+FLAGS: premapped
+GEOMETRY:left,top
+MAP
+|||||||||||||||
+|.I.L.........|
+|I....L.....L.|
+|L...I........|
+|.L.......L..I|
+|...LL..L.....|
+|.I.........L.|
+|....L..I.....|
+|.L........L.I|
+|..L..........|
+|..I....L...II|
+|....L..I.I...|
+|.L.......L...|
+|.....I.......|
+|||||||||||||||
+ENDMAP
+STAIR:(13, 13),down
+BRANCH: (1,1,1,1),(2,2,2,2)
+REGION: (0,0,20,80), lit, "ordinary"
+"""
