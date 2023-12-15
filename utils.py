0d53cff539f450664a3d12f735c4fbc046484219
VeronicaPistolesi <v.pistolesi6@studenti.unipi.it>
2023-12-15 18:49:28 +0100

    matrix and coordinates functions

--- utils.py
+++ utils.py
@@ -30,3 +30,28 @@ def actions_from_path(start: Tuple[int, int], path: List[Tuple[int, int]]) -> Li
         y_s = y
     
     return actions
+
+# ------------------------------------------------------------------------------------------------------------
+
+def process_matrix(matrix):
+    chars = {32, 0}
+    new_matrix = [[element for element in row if element not in chars] for row in matrix] #Removes 32 values from the matrix
+    new_matrix = [row for row in new_matrix if row]  #Removes empty rows    
+    new_matrix = np.array(new_matrix) #Convert to NumPy array
+    
+    return new_matrix
+
+def find_state_coord(self, value):
+        for y in range(len(self)):  # iteration over the rows of the 2-dimensional array, len(self) = number of rows
+            for x in range(len(self[0])):   # iteration over columns; len(self[0]) = gives number of columns
+                if self[y][x] == value:     # check current position in array equals to value
+                    return (x, y)   # tuple containing the coordinates
+        return None
+
+def find_lava_coord(self, value):
+        lava_pos = []
+        for y in range(len(self)):
+            for x in range(len(self[0])):
+                if self[y][x] == value:
+                    lava_pos.append((x, y))
+        return lava_pos
