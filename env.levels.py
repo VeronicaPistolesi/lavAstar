3eea439f43df2fd6c3c428cc881973c787c2df28
sofija711 <sofijakotur@icloud.com>
2023-12-15 19:05:59 +0100

    Level 2

--- env.levels.py
+++ env.levels.py
@@ -26,3 +26,29 @@ STAIR:(13, 13),down
 BRANCH: (1,1,1,1),(2,2,2,2)
 REGION: (0,0,20,80), lit, "ordinary"
 """
+#LEVEL 2
+des_file_static_lv2 = """
+MAZE: "mylevel", ' '
+FLAGS: premapped
+GEOMETRY:left,top
+MAP
+|||||||||||||||
+|...L.........|
+|.....L.....L.|
+|L............|
+|.L.......L...|
+|...LL..L.....|
+|..L........L.|
+|....L........|
+|.L........L..|
+|..L........L.|
+|.......L...L.|
+|....L......L.|
+|.L.......L..L|
+|..........L..|
+|||||||||||||||
+ENDMAP
+STAIR:(13, 13),down
+BRANCH: (1,1,1,1),(2,2,2,2)
+REGION: (0,0,20,80), lit, "ordinary"
+"""
