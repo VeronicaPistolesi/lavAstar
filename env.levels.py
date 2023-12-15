27ccce4fcab809a9a62a9bc4afabf9f45fd4558b
matildeec <cmatilde01@gmail.com>
2023-12-15 19:10:49 +0100

    level 4 dynamic

--- env.levels.py
+++ env.levels.py
@@ -79,3 +79,60 @@ STAIR:(13, 13),down
 BRANCH: (1,1,1,1),(2,2,2,2)
 REGION: (0,0,20,80), lit, "ordinary"
 """
+
+> Derya G.:
+#------------------------------------------------
+#LEVEL 3
+des_file_static_lv3 = """
+MAZE: "mylevel", ' '
+FLAGS: premapped
+GEOMETRY:left,top
+MAP
+|||||||||||||||
+|...L....LL...|
+|..LL.L.....L.|
+|L.....LLL....|
+|.L..L....L...|
+|...LL.L......|
+|L......LL..L.|
+|..L.L........|
+|.L....LL...L.|
+|..L..L...LL..|
+|LL.....L..L..|
+|...LLL......L|
+|.L.....LL...L|
+|...LL.....L..|
+|||||||||||||||
+ENDMAP
+STAIR:(13, 13),down
+BRANCH: (1,1,1,1),(2,2,2,2)
+REGION: (0,0,20,80), lit, "ordinary"
+"""
+
+#LEVEL 4 - DYNAMIC
+des_file_static_lv4 = """
+MAZE: "mylevel", ' '
+FLAGS: premapped
+GEOMETRY:left,top
+MAP
+|||||||||||||||
+|...L.........|
+|.....L.....L.|
+|L............|
+|.L.......L...|
+|...LL..L.....|
+|...........L.|
+|....L........|
+|.L........L..|
+|..L..........|
+|.......L.....|
+|....L........|
+|.L.......L...|
+|.............|
+|||||||||||||||
+ENDMAP
+STAIR:(13, 13),down
+BRANCH: (1,1,1,1),(2,2,2,2)
+MONSTER: ('H', "giant"), random, hostile
+REGION: (0,0,20,80), lit, "ordinary"
+"""
